60cf4fbe90a730fc74225e92d5c9ba743b85d681
forthefuture05 <patrik25052005@gmail.com>
2024-11-12 22:30:55 +0100

    2nd try

--- config/database.js
+++ config/database.js
@@ -1,9 +1,26 @@
 module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-        client: 'postgres',
-        connection: {
-            connectionString: env('DATABASE_PRIVATE_URL')
-        },
-        pool: { min: 0 }
-    }
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE', 'strapi'),
+      user: env('PGUSER', 'strapi'),
+      password: env('PGPASSWORD', 'password'),
+      ssl: env.bool(true),
+    },
+    pool: { min: 0 }
+  },
 });
+
+// const path = require("path");
+
+// module.exports = () => ({
+//   connection: {
+//     client: "sqlite",
+//     connection: {
+//       filename: path.join(__dirname, "..", ".tmp/data.db"),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
